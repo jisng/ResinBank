8fca71dc7d9cfa01027688d4c43cb66e1f00fbc8
jisng <jxsxxng@gmail.com>
2023-09-28 15:50:08 +0900

    create project

---
+++ ResinBank/ResinBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResinBank
+//
+//  Created by 박지승 on 2023/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
